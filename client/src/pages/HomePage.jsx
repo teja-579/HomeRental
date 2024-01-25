9e36d1aceaadf3e76382d368c0afe762a1c65323
Ashish <ashishrath1997@gmail.com>
2024-01-26 04:29:55 +0530

    add slide and listing in homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx';
+import Slide from '../components/Slide.jsx';
+import Categories from '../components/Categories.jsx';
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
+      <Slide />
+      <Categories />
     </>
   )
 }
