895dc1e206974978a29732e86e7bca99a55cbda2
Ashish <ashishrath1997@gmail.com>
2024-01-26 04:08:52 +0530

    navbar complete

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,10 +4,13 @@ import { Search, Person, Menu } from '@mui/icons-material';
 import { useSelector, useDispatch } from "react-redux";
 import variables from '../styles/variables.scss';
 import '../styles/Navbar.scss';
+import { Link, useNavigate } from "react-router-dom";
+import { setLogout } from "../redux/state";
 
 const Navbar = () => {
   const [dropdownMenu, setDropdownMenu] = useState(false);
   const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   return (
     <div className='navbar'>
         <a href="/">
@@ -24,8 +27,8 @@ const Navbar = () => {
         </div>
 
         <div className='navbar_right'>
-          {user ? (<a href='/create-listing'>Become A Host</a>) : (<a href='/login'>Become A Host</a>)}
-          <button className='navbar_right_account'>
+          {user ? (<a href='/create-listing' className='host'>Become A Host</a>) : (<a href='/login' className='host'>Become A Host</a>)}
+          <button className='navbar_right_account' onClick={() => setDropdownMenu(!dropdownMenu)}>
             <Menu sx={{ color: variables.darkgrey }} />
             {!user ? (
               <Person sx={{ color: variables.darkgrey }} />
@@ -33,6 +36,32 @@ const Navbar = () => {
               <img src={`http://localhost:7000/${user.profileImagePath.replace("public", "")}`} alt='profile photo' style={{ objectFit: "cover", borderRadius: "50%"}}/>
             )}
           </button>
+
+          {dropdownMenu && !user && (
+            <div className='navbar_right_accountmenu'>
+              <Link to='/login'>Log In</Link>
+              <Link to='/register'>Sign Up</Link>
+            </div>
+          )}
+
+          {dropdownMenu && user && (
+            <div className="navbar_right_accountmenu">
+              <Link to={`/${user._id}/trips`}>Trip List</Link>
+              <Link to={`/${user._id}/wishList`}>Wish List</Link>
+              <Link to={`/${user._id}/properties`}>Property List</Link>
+              <Link to={`/${user._id}/reservations`}>Reservation List</Link>
+              <Link to="/create-listing">Become A Host</Link>
+
+              <Link
+                to="/login"
+                onClick={() => {
+                  dispatch(setLogout());
+                }}
+              >
+                Log Out
+              </Link>
+            </div>
+        )}
         </div>
     </div>
   )
